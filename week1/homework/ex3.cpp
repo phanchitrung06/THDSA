9f54b5d529ecb5041ddbe1d75e47f47a314a7217
phanchitrung06 <phanchitrung01.st@gmail.com>
2025-03-20 21:38:36 +0700

    week1

--- week1/homework/ex3.cpp
+++ week1/homework/ex3.cpp
@@ -10,6 +10,10 @@ int main()
 	string str="";
 	 generateBinaryStrings( n,  str);
 }
+// Phuong phap de quy, quay lui
+// Ta se lap tung gia tri 0 den 1 de gan vao chuoi str va so sanh do dai chuoi str voi n
+// Neu do dai chuoi str < n se goi de quy generateBinaryStrings(n, str) de gan them 0 hoac 1 de tang them do dai chuoi str
+// Neu do dai chuoi str = n se in cau hinh va quay lui
 
 void generateBinaryStrings(int n, string str)
 {
@@ -30,7 +34,7 @@ void generateBinaryStrings(int n, string str)
 		str.erase(str.length() - 1);
 	}
 }
-
+// in cau hinh nhi phan ra
 void in( string str)
 {
 	for (int i = 0; i<str.length(); i++)
