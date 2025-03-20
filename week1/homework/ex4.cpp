86ee1cc129f0520b30af73f67c0188a97a74bfc7
phanchitrung06 <phanchitrung01.st@gmail.com>
2025-03-20 21:39:05 +0700

    week1

--- week1/homework/ex4.cpp
+++ week1/homework/ex4.cpp
@@ -9,6 +9,12 @@ int main()
 	
 		process(n, 'A', 'B', 'C');
 }
+// Phuong phap de quy
+// Neu n = 1 thi ta se chuyen truc tiep tu cot A sang cot C
+// Neu n > 1:
+//   - Dau tien, ta co n so dia o vi tri cot A, ta can chuyen n - 1 cai dia o cot A sang cot B voi cot C lam trung gian bang cach goi lai ham process(n - 1, A, C, B)
+//   - Ta se chuyen 1 dia con lai o vi tri cot A sang cot C bang cach goi lai ham process(1, A, B, C)
+//   - Cuoi cung, ta se chuyen n - 1 cai dia tu vi tri cot B sang cot C voi cot A lam trung gian bang cach goi lai ham process(n - 1, B, A, C)
 
 void process(int n, char nguon, char trunggian, char dich)
 {
