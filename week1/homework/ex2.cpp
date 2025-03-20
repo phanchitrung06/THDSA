f365c471f8fe22d6564eca17d0124297cd56cc8d
phanchitrung06 <phanchitrung01.st@gmail.com>
2025-03-20 21:37:47 +0700

    week1

--- week1/homework/ex2.cpp
+++ week1/homework/ex2.cpp
@@ -9,12 +9,14 @@ int main()
 	cout<< factorial(n);
 }
 // phuong phap su dung de quy
+// Neu n=0 tra ve 0
 // Neu n = 1 tra ve 1
 // Neu n >= 2, tinh factorial(n) = n * factorial(n - 1) bang cach goi de quy
 // Va in ra gia tri tu giai thua thu n
 
 int factorial(int n)
 {
+	if (n==0) return 0;
 	if (n == 1) return 1;
 	return n * factorial(n - 1);
 }
